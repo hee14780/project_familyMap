6ec57b72102a77684aa6c3c65659961ddd817ed7
hee14780 <hee14780@doumfactory.com>
2020-03-13 15:09:53 +0900

    세대이동버튼 스타일 수정

--- fmap3.js
+++ fmap3.js
@@ -287,7 +287,7 @@ var family = new Vue({
                         // 컨텐츠 가운데 정렬 (윈도우이너높이 - 헤더높이) / 2 - 세대높이
                         let contMargin = (window.innerHeight - 83) / 2 - 193;
 
-                        $(".group.treeWrap").css("margin-top", contMargin + "px");
+                        $(".group.treeWrap").css("padding-top", contMargin + "px");
                         $(".content > .btn_wrap").css("top", contMargin + 83 + "px");
                         console.log("scrollWidth", this.$refs.treeWrap.scrollWidth);
                     }, 50);
